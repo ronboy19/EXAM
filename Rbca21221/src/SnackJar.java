f84e179602323ed7c6ceebf13bcb1d99b7fa37f7
admin <admin@Bcalab4-m11>
2023-02-25 10:32:23 +0530

    2nd commit

--- Rbca21221/src/SnackJar.java
+++ Rbca21221/src/SnackJar.java
@@ -1,12 +1,17 @@
 
 public class SnackJar {
 int num_biscuits;
-SnackJar(int num_biscuits){
+String Stringjar_color;
+SnackJar(int num_biscuits,String Stringjar_color){
 	this.num_biscuits=num_biscuits;
+	this.Stringjar_color=Stringjar_color;
 }
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      SnackJar sj=new SnackJar(30);
+      SnackJar sj=new SnackJar(30,"blue");
+          
 	}
 
 }
