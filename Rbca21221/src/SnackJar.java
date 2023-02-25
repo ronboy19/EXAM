06252ae56c082bea9e5df3bec6b6919666e6ab51
admin <admin@Bcalab4-m11>
2023-02-25 10:22:14 +0530

    First commit

---
+++ Rbca21221/src/SnackJar.java
@@ -0,0 +1,12 @@
+
+public class SnackJar {
+int num_biscuits;
+SnackJar(int num_biscuits){
+	this.num_biscuits=num_biscuits;
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      SnackJar sj=new SnackJar(30);
+	}
+
+}
